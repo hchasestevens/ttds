de208a9884644e7c1171a2a487e30aaf0e4eb0c8
H. Chase Stevens <chase@chasestevens.com>
2014-10-19 16:15:02 +0100

    best.py - further adjust stopword thresh for accuracy

--- tts2/best.py
+++ tts2/best.py
@@ -69,7 +69,7 @@ def indexed_cos_tfidf(get_idf, q_tokens, q_denom, token_index, d_denoms):
     )
     return doc, score
 
-N = 50000
+N = 10000
 
 def main():
     with open('news.idf', 'r') as f:
@@ -99,7 +99,7 @@ def main():
         'can', 'will', 'just', 'don', 'should', 'now'
     ))  # taken from nltk.corpus.stopwords.words('english')
 
-    news_stopwords = set(word for word, value in idf_scores.iteritems() if value < 3.4951)
+    news_stopwords = set(word for word, value in idf_scores.iteritems() if value < 3.38211)
 
     stopwords = frozenset(english_stopwords | news_stopwords)
     print len(stopwords)
