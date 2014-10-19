97574d1f7b836555ac7f63afe93f4be7b2b11be9
H. Chase Stevens <chase@chasestevens.com>
2014-10-19 14:54:12 +0100

    best.py - increase news_stopword threshold to 3.4951 (same accuracy)

--- tts2/best.py
+++ tts2/best.py
@@ -99,7 +99,7 @@ def main():
         'can', 'will', 'just', 'don', 'should', 'now'
     ))  # taken from nltk.corpus.stopwords.words('english')
 
-    news_stopwords = set(word for word, value in idf_scores.iteritems() if value < 3.45)
+    news_stopwords = set(word for word, value in idf_scores.iteritems() if value < 3.4951)
 
     stopwords = frozenset(english_stopwords | news_stopwords)
     in_stopwords = stopwords.__contains__
